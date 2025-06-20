0c76ff05de9befca91b8ee8adb4c634b12f6cf08
hrtjsingh <42902290+hrtjsingh@users.noreply.github.com>
2025-06-20 16:50:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://backend-lwao.onrender.com:3000',
+        target: 'https://backend-lwao.onrender.com',
         changeOrigin: true,
       },
     },
