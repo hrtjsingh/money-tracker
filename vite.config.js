4061cf9c84f05e4eb949a23257ff21c0582fd1b3
Hartej Singh <hrtjsingh13@gmail.com>
2025-06-20 16:31:47 +0530

    backend url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://backend-lwao.onrender.com:3000',
         changeOrigin: true,
       },
     },
